e5ba3d7c8adbda0bdcb1f899faf78f812b24ea1f
s1rusOne <forjbi@mail.ru>
2024-10-11 20:42:39 +0300

    Project Management API

--- obj/Debug/net8.0/ProjectManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d0f495fb65d4f704807139076d900b81d9cc12")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
